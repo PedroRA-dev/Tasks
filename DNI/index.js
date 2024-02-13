5ab4b96274109b635df047fcfc3990dd47fcf7d5
Pedro Robles <pedroroblesdev@gmail.com>
2024-02-13 18:31:38 +0100

    DNI

---
+++ DNI/index.js
@@ -0,0 +1,35 @@
+// entrada del usuario
+
+const calcularLetra = (numeroDNI) => {
+    let letra = letrasDNI.charAt(numeroDNI % 23)
+    return letra
+}
+let letrasDNI = 'TRWAGMYFPDXBNJZSQVHLCKE'
+
+const input = prompt('introduce tu numero DNI');
+
+if(input.length === 0) {
+    alert('No has introducido nada')
+}
+else if(isNaN(input)) {
+    alert('No has introducido un numero')
+}
+else if(input.length !== 8) {
+    alert('No as escrito bien el DNI')
+}
+else {
+    let letraDNI = calcularLetra(input)
+    alert(`La letra del DNI es ${letraDNI}`)
+
+}
+
+
+// que el modulo no supere 23
+
+
+
+
+
+
+
+
